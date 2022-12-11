2f5e334c3ba7fa63750b96b915ec08bed2a83985
mochyusufefendi <yusuffe92@gmail.com>
2022-12-11 18:21:50 +0700

    update api

---
+++ mobile-test-capstone/src/test/java/com/example/steps/registerStep.java
@@ -0,0 +1,52 @@
+package com.example.steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class registerStep {
+    @Given("I am on the login page")
+    public void iAmOnTheLoginPage() {
+    }
+
+    @When("I click register button")
+    public void iClickRegisterButton() {
+    }
+
+    @And("I input nama {string}")
+    public void iInputNama(String arg0) {
+    }
+
+    @And("I input nik {string}")
+    public void iInputNik(String arg0) {
+    }
+
+    @And("I input tanggal lahir {string}")
+    public void iInputTanggalLahir(String arg0) {
+    }
+
+    @And("I input alamat {string}")
+    public void iInputAlamat(String arg0) {
+    }
+
+    @And("I input email {string}")
+    public void iInputEmail(String arg0) {
+    }
+
+    @And("I input jenis kelamin {string}")
+    public void iInputJenisKelamin(String arg0) {
+    }
+
+    @And("I input password {string}")
+    public void iInputPassword(String arg0) {
+    }
+
+    @And("I click register")
+    public void iClickRegister() {
+    }
+
+    @Then("I succes create my account")
+    public void iSuccesCreateMyAccount() {
+    }
+}
